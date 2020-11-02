5f1e3b6b3856892036d8bf51b12205927e74e3bc
Shreyash Pandey <pandeyshreyash2201@gmail.com>
2020-11-02 09:41:47 +0530

    First commit

---
+++ screens/BookDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BookDetail = () => {
+  return (
+    <View>
+      <Text>Books</Text>
+    </View>
+  );
+};
+
+export default BookDetail;
+
+const styles = StyleSheet.create({});
---
+++ screens/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+import { COLORS, icons, images, FONTS, SIZES } from "../constants";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
